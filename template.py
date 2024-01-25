9b63764c429b2c41e2ea9485d0aeac59bbbbe040
sanshekh <shekharsandeep8@gmail.com>
2024-01-25 19:31:03 +0530

    Repo updates

--- template.py
+++ template.py
@@ -1,39 +1,46 @@
 import os
 from pathlib import Path 
+import logging
+
+logging.basicConfig(level = logging.INFO, format= '[%(asctime)s]: %(message)s: ')
+
+project_name = "ML_OPS"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "src/__init__.py",
-    "src/components/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py"
-    "src/utils/utils.py",
-    "src/logger/logging.py",
-    "src/exception/exception",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
     "requirements.txt",
-    "requirements_dev.txt",
     "setup.py",
-    "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
+    "templates/index.html",
     "experiments/experiment.ipynb"   
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
+
     if filedir != "":
         os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # Empty file creation
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists.")
